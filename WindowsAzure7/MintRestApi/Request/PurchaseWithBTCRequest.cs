cd994794767cd83a5c62c1b69780eb2f718bfff6
SQJ <shenqijun417@sjtu.edu.cn>
2014-09-10 14:58:22 +0800

    Request

---
+++ WindowsAzure7/MintRestApi/Request/PurchaseWithBTCRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MintRestApi
+{
+    [DataContract]
+    public class PurchaseWithBTCRequest
+    {
+        [DataMember]
+        public string email;
+
+        [DataMember]
+        public string type;
+
+        [DataMember]
+        public string id;
+
+        [DataMember]
+        public string token_value;
+
+        [DataMember]
+        public string token;
+
+    }
+}
